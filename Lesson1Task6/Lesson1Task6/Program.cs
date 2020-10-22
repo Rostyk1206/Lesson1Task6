2c249362c4837d8792f11672e32be5e7efddbc0a
unknown <rostykvoznyak@gmail.com>
2020-10-22 23:10:42 +0300

    Lesson1Task6

---
+++ Lesson1Task6/Lesson1Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson1Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool married = false;
+            Console.WriteLine("IsMarried:{0}",married);
+        }
+    }
+}
